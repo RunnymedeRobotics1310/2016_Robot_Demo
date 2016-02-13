701867b7063319e01823d300f4a03d870f3dce33
AquaBytez <anddypena@outlook.com>
2016-02-13 10:52:26 -0500

    Added Windup Command

---
+++ src/robot/commands/WindupCommand.java
@@ -0,0 +1,28 @@
+package robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import robot.Robot;
+
+public class WindupCommand extends Command {
+
+    public WindupCommand() {
+    	requires(Robot.shooterSubsystem);
+    }
+
+    protected void initialize() {
+    }
+
+    protected void execute() {
+    	Robot.shooterSubsystem.startShooterMotor();
+    }
+
+    protected boolean isFinished() {
+        return Robot.shooterSubsystem.getShooterSpeed() > 100;
+    }
+
+    protected void end() {
+    }
+
+    protected void interrupted() {
+    }
+}

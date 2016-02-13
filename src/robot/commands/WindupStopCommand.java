8f897d3bfe919e1b3022ed131c2e88c67fcce849
richardemcmullin <richard.e.mcmullin@gmail.com>
2016-02-13 11:19:22 -0500

    Shooter stop

--- src/robot/commands/WindupStopCommand.java
+++ src/robot/commands/WindupStopCommand.java
@@ -3,14 +3,14 @@ package robot.commands;
 import edu.wpi.first.wpilibj.command.Command;
 import robot.Robot;
 
-public class WindupCommand extends Command {
+public class WindupStopCommand extends Command {
 
-    public WindupCommand() {
+    public WindupStopCommand() {
     	requires(Robot.shooterSubsystem);
     }
 
     protected void initialize() {
-    	Robot.shooterSubsystem.startShooterMotor();
+    	Robot.shooterSubsystem.stopShooterMotor();
     }
 
     protected void execute() {

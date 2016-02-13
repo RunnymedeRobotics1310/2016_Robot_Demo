576878969525ef55c4b37eec8f2adbcdd97450dc
chand <chand@10.17.192.147>
2016-02-13 10:55:31 -0500

    Added LowShotCommand

---
+++ src/robot/commands/LowShotCommand.java
@@ -0,0 +1,39 @@
+package robot.commands;
+import edu.wpi.first.wpilibj.command.Command;
+import robot.Robot;
+import robot.subsystems.ShooterSubsystem.IntakeReverseSpeed;
+
+public class LowShotCommand extends Command {
+
+	public LowShotCommand() {
+		this.setTimeout(2.0);
+	}
+
+	@Override
+	protected void execute() {
+		Robot.shooterSubsystem.intakeMotorReverse(IntakeReverseSpeed.HIGH);
+	}
+
+	@Override
+	protected void initialize() {
+		Robot.shooterSubsystem.intakeEncoderReset();
+	}
+	
+	@Override
+	protected void end() {
+		Robot.shooterSubsystem.stopIntakeMotor();
+
+	}
+	
+	@Override
+	protected boolean isFinished() {
+		return isTimedOut();
+	}
+
+	@Override
+	protected void interrupted() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}

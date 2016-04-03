36cecc8ac3d45d3a3d605d8c093cff7d8afe92da
Chris L <christopher.lansdale@gmail.com>
2016-04-03 15:52:30 -0400

    Pivot To Angle
    Copied and modified the GoStraightCommand to pivot in place.

---
+++ src/robot/commands/drive/PivotToAngleCommand.java
@@ -0,0 +1,79 @@
+package robot.commands.drive;
+
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import robot.Robot;
+import robot.pids.PivotPID;
+
+/**
+ *
+ */
+public class PivotToAngleCommand extends Command {
+
+	double angleSetpoint;
+
+	public PivotToAngleCommand(double angle) {
+		requires(Robot.chassisSubsystem);
+		this.angleSetpoint = angle;
+	}
+
+	// Called just before this Command runs the first time
+	protected void initialize() {
+		PivotPID.setEnabled(false);
+		PivotPID.setSetpoint(angleSetpoint);
+		PivotPID.setEnabled(true);
+	}
+
+	// Called repeatedly when this Command is scheduled to run
+	protected void execute() {
+		
+		double leftSpeed;
+		double rightSpeed;
+
+		SmartDashboard.putNumber("Angle setpoint", angleSetpoint);
+		SmartDashboard.putNumber("Angle difference", -Robot.chassisSubsystem.getAngleDifference(angleSetpoint));
+		SmartDashboard.putNumber("AnglePIDOutput", PivotPID.getOutput());
+
+		double turn = PivotPID.getOutput();
+		
+		if(turn > 0) {
+			leftSpeed = turn;
+			rightSpeed = 0;
+		}
+		else if (turn < 0) {
+			leftSpeed = 0;
+			rightSpeed = -turn;
+		}
+		else {
+			leftSpeed = turn;
+			rightSpeed = -turn;
+		}
+
+		Robot.chassisSubsystem.setSpeed(leftSpeed, rightSpeed);
+	}
+
+	// Make this return true when this Command no longer needs to run execute()
+	protected boolean isFinished() {
+		double turn = Robot.oi.getTurn();
+		if (Math.abs(turn) > 0.03) {
+			return true;
+		}
+		return false;
+
+	}
+
+	// Called once after isFinished returns true
+	protected void end() {
+		PivotPID.setEnabled(false);
+		/*
+		 * Note: added motor stop incase it's not called by a separate command
+		 * as in AutoGoStraightCommand calling GoStraightCommand
+		 */
+		Robot.chassisSubsystem.setSpeed(0, 0);
+	}
+
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	protected void interrupted() {
+	}
+}
